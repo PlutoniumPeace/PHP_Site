4de6f5bc424358cf9e60d6566f68367e9772f9dd
jhampton20 <99155280+jhampton20@users.noreply.github.com>
2022-04-06 23:47:21 -0500

    Update Goodbye.php

--- Hr12_13/Goodbye.php
+++ Hr12_13/Goodbye.php
@@ -4,6 +4,11 @@ Juwan Hampton
 CSC155-201H -->
 <head>
 <title>Goodbye!</title>
+<?php
+session_start();
+session_destroy();
+header("refresh: 5, url=log_in.php");
+?>
 </head>
 <body>
 <center><b>YOU HAVE BEEN LOGGED OUT. CLICK BELOW TO LOGIN AGAIN</b></center>
